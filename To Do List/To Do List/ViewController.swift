720fbf40b5029f9ed1edc60df8ea6b92431bb89d
Alex Shabalin <shabalin.media@gmail.com>
2022-08-17 22:51:51 +0600

    Works but needs delet functionality.

--- To Do List/To Do List/ViewController.swift
+++ To Do List/To Do List/ViewController.swift
@@ -28,15 +28,26 @@ class ViewController: UIViewController {
             UserDefaults().set(0, forKey: "count")
         }
         
+    }
+        
         func updateTasks() {
+            
+            tasks.removeAll()
             
             guard let count = UserDefaults().value(forKey: "count") as? Int else {
                 return
             }
             
+            for x in 0..<count {
+                if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                    tasks.append(task)
+                }
+            }
+            
+            tableView.reloadData()
+            
         }
 
-    }
 
     @IBAction func didTapAdd() {
         let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
@@ -54,6 +65,15 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task = tasks[indexPath.row]
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
     }
 }
 
