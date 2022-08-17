ecaae384e6e8effb3a3d9da28901f670fe420633
Alex Shabalin <shabalin.media@gmail.com>
2022-08-17 22:16:27 +0600

    First Commit.

---
+++ To Do List/To Do List/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  To Do List
+//
+//  Created by Alexander Shabalin on 17/8/22.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
